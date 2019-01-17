dd1adafc14b973e64f425152a0f6dacea5e0d24c
Ed Courtney <edcourtney74@gmail.com>
2019-01-16 22:01:59 -0500

    Updates

---
+++ precache-manifest.d2a8ecd9e00cff4e08545459ccbcad40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab782b88b5fa2c4ef1f2",
+    "url": "/Simpsons-Memory-Game/static/js/runtime~main.ab782b88.js"
+  },
+  {
+    "revision": "9cba031c7d5539e50501",
+    "url": "/Simpsons-Memory-Game/static/js/main.9cba031c.chunk.js"
+  },
+  {
+    "revision": "46b1084bfdcac49c9b0e",
+    "url": "/Simpsons-Memory-Game/static/js/1.46b1084b.chunk.js"
+  },
+  {
+    "revision": "9cba031c7d5539e50501",
+    "url": "/Simpsons-Memory-Game/static/css/main.ef57f183.chunk.css"
+  },
+  {
+    "revision": "0c3ca157e6705f433d003bd5ca1c60c9",
+    "url": "/Simpsons-Memory-Game/index.html"
+  }
+];
